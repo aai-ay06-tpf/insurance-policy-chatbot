5eebfca8b00ce6649cff08c85aee7c4810ed3090
jackonedev <jackone.action.software@gmail.com>
2024-03-03 16:44:14 -0300

    updated project

--- frontend/utils/config.py
+++ frontend/utils/config.py
@@ -2,6 +2,6 @@ import os
 
 SERVICE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SERVICE_NAME = os.path.basename(SERVICE_PATH)
-SERVICE_2_NAME = "backend"
-SERVICE_2_PATH = os.path.join(os.path.dirname(SERVICE_PATH), SERVICE_2_NAME)
-RETRIEVER_PATH = os.path.join(SERVICE_2_PATH, "main_retriever.pkl")
+SERVICE_1_NAME = "backend"
+SERVICE_1_PATH = os.path.join(os.path.dirname(SERVICE_PATH), SERVICE_1_NAME)
+RETRIEVERS_PATH = os.path.join(SERVICE_1_PATH, "pdf_dict.pkl")
