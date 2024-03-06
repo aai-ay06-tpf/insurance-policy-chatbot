aeeb8dd45bb6e2c123362d45b5dc1a07aeb30f95
jackonedev <jackone.action.software@gmail.com>
2024-03-06 18:03:48 -0300

    updated system files

--- frontend/utils/config.py
+++ frontend/utils/config.py
@@ -1,10 +1,9 @@
 import os
 
+ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 SERVICE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SERVICE_NAME = os.path.basename(SERVICE_PATH)
-SERVICE_1_NAME = "backend"
-SERVICE_1_PATH = os.path.join(os.path.dirname(SERVICE_PATH), SERVICE_1_NAME)
-VDB_PATH = os.path.join(SERVICE_1_PATH, "vdb_qdrant")
-QVDB_BASE_PATH = os.path.join(SERVICE_PATH, VDB_PATH, "{filename}")
+
+QVDB_BASE_PATH = os.path.join(ROOT_PATH, "vdb_qdrant", "{filename}")
 
 
