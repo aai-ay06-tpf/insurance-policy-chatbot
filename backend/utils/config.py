230f035295e4f7fe73564af7d0c1b88540882d60
jackonedev <jackone.action.software@gmail.com>
2024-03-07 17:03:59 -0300

    updated system files

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -6,9 +6,6 @@ def read_config(parser: ConfigParser, location: str) -> None:
     assert parser.read(location), f"Could not read config {location}"
 
 
-def get_config(parser: ConfigParser, section: str, key: str) -> str:
-    return parser.get(section, key)
-
 env_config = ConfigParser()
 CONFIG_FILE = os.path.join(os.getcwd(), '.env')
 read_config(env_config, CONFIG_FILE)
@@ -19,17 +16,17 @@ SERVICE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SERVICE_NAME = os.path.basename(SERVICE_PATH)
 
 # AWS S3 Configuration
-ACCESS_KEY = get_config(env_config, 'aws-s3', 'ACCESS_KEY')
-SECRET_KEY = get_config(env_config, 'aws-s3', 'SECRET_KEY')
-DATASET_PATH = get_config(env_config, 'aws-s3', 'DATASET_PATH')
-BUCKET_NAME = get_config(env_config, 'aws-s3', 'BUCKET_NAME')
-prefix = get_config(env_config, 'aws-s3', 'prefix')
+ACCESS_KEY = env_config.get('aws-s3', 'ACCESS_KEY')
+SECRET_KEY = env_config.get('aws-s3', 'SECRET_KEY')
+DATASET_PATH = env_config.get('aws-s3', 'DATASET_PATH')
+BUCKET_NAME = env_config.get('aws-s3', 'BUCKET_NAME')
+prefix = env_config.get('aws-s3', 'prefix')
 
 # AWS S3 PDF download path
 DOWNLOAD_PATH = os.path.join(SERVICE_PATH, 'database_s3', 'queplan_insurance')
 os.makedirs(DOWNLOAD_PATH, exist_ok=True)
 
 # Qdrant Vector Database Path 
-QVDB_BASE_PATH = os.path.join(ROOT_PATH, "vdb_qdrant", "{filename}")
-os.makedirs(os.path.split(QVDB_BASE_PATH)[0], exist_ok=True)
+QVDB_BASE_PATH = os.path.join(ROOT_PATH, "vdb_qdrant", "{db_name}")
+
 
--- frontend/utils/config.py
+++ frontend/utils/config.py
@@ -4,6 +4,6 @@ ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fi
 SERVICE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SERVICE_NAME = os.path.basename(SERVICE_PATH)
 
-QVDB_BASE_PATH = os.path.join(ROOT_PATH, "vdb_qdrant", "{filename}")
+QVDB_BASE_PATH = os.path.join(ROOT_PATH, "vdb_qdrant", "{db_name}")
 
 
