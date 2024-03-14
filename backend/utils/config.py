92e582ba81130c82cb8850ce812f6344faa8d937
jackonedev <jackone.action.software@gmail.com>
2024-03-14 16:43:55 -0300

    updated backend system files

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -14,6 +14,10 @@ ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fi
 SERVICE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SERVICE_NAME = os.path.basename(SERVICE_PATH)
 
+
+QDRANT_HOST = env_config.get('qdrant', 'QDRANT_HOST')
+QDRANT_PORT = env_config.get('qdrant', 'QDRANT_PORT')
+
 QDRANT_URL = env_config.get('qdrant', 'QDRANT_URL')
 QDRANT_LOCAL_PATH = os.path.join(ROOT_PATH, "qdrant_collections")
 
