b989f70d8dfec0888358e49a37cbd4ec7b0ecce6
jackonedev <jackone.action.software@gmail.com>
2024-03-15 16:48:19 -0300

    updated scrapper with ParentDocumentRetriever working

--- backend_ui/ml_service/web_scrapper.py
+++ backend_ui/ml_service/web_scrapper.py
@@ -1,5 +1,6 @@
 
 from concurrent.futures import ThreadPoolExecutor
+from copy import copy
 
 from ml_service.webscrapper_codigos_chile import web_documents, web_vectorstore
 from ml_service.webscrapper_codigos_chile.web_parent_document_retriever import obtain_parentdocument_retriever
@@ -32,6 +33,7 @@ def obtain_docs_executed():
 
 
 def qdrant_retriever(searchs, search_type="similarity", kwargs=None):
+    global all_docs
     """
 
     Params:
@@ -46,6 +48,8 @@ def qdrant_retriever(searchs, search_type="similarity", kwargs=None):
 
     if not obtain_docs_executed():
         all_docs = obtain_docs(searchs)
+    else:
+        all_docs = copy(all_docs)
 
     vectorstore = web_vectorstore.obtain_vectorstore_from_docs(all_docs)
 
@@ -56,46 +60,12 @@ def qdrant_retriever(searchs, search_type="similarity", kwargs=None):
     return vectorstore.as_retriever(search_type=search_type)
 
 
-"""
-Examples:
-
-# Retrieve more documents with higher diversity
-# Useful if your dataset has many similar documents
-docsearch.as_retriever(
-    search_type="mmr",
-    search_kwargs={'k': 6, 'lambda_mult': 0.25}
-)
-
-# Fetch more documents for the MMR algorithm to consider
-# But only return the top 5
-docsearch.as_retriever(
-    search_type="mmr",
-    search_kwargs={'k': 5, 'fetch_k': 50}
-)
-
-# Only retrieve documents that have a relevance score
-# Above a certain threshold
-docsearch.as_retriever(
-    search_type="similarity_score_threshold",
-    search_kwargs={'score_threshold': 0.8}
-)
-
-# Only get the single most similar document from the dataset
-docsearch.as_retriever(search_kwargs={'k': 1})
-
-# Use a filter to only retrieve documents from a specific paper
-docsearch.as_retriever(
-    search_kwargs={'filter': {'paper_title':'GPT-4 Technical Report'}}
-)
-
-Ref:
-* https://api.python.langchain.com/en/latest/vectorstores/langchain_community.vectorstores.qdrant.Qdrant.html#langchain_community.vectorstores.qdrant.Qdrant.as_retriever
-"""
-
-
 def parent_document_retriever(searchs):
-    print("DOESN'T WORK: TypeError: Can't instantiate abstract class TextSplitter with abstract method split_text")
+    global all_docs
+    
     if not obtain_docs_executed():
         all_docs = obtain_docs(searchs)
+    else:
+        all_docs = copy(all_docs)
 
     return obtain_parentdocument_retriever(all_docs)
--- backend_ui/ml_service/webscrapper_codigos_chile/web_parent_document_retriever.py
+++ backend_ui/ml_service/webscrapper_codigos_chile/web_parent_document_retriever.py
@@ -2,37 +2,38 @@ from langchain_community.vectorstores.chroma import Chroma
 from langchain.retrievers.parent_document_retriever import ParentDocumentRetriever
 from langchain.storage import InMemoryStore
 from langchain_text_splitters.character import CharacterTextSplitter, RecursiveCharacterTextSplitter
-# from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_text_splitters.base import TextSplitter
 
 from ml_service.tools.embeddings import Embeddings
+from utils.config import QDRANT_URL
+
 
 def obtain_parentdocument_retriever(docs):
     
-    print("doesn't work: TypeError: Can't instantiate abstract class TextSplitter with abstract method split_text")
 
-    # child_splitter = CharacterTextSplitter(
-    child_splitter = TextSplitter.from_tiktoken_encoder(
+    # It should create documents smaller than the parent
+    child_splitter = CharacterTextSplitter.from_tiktoken_encoder(
         chunk_size=100, chunk_overlap=20
-    )#.split_documents(documents=docs)
+    )
 
     emb = Embeddings()
-    embeddings = emb.obtain_embeddings("ada_embeddings")
+    embeddings = emb.obtain_embeddings("openai_embeddings")
 
     # The vectorstore to use to index the child chunks
     vectorstore = Chroma(
         collection_name="all_documents", embedding_function=embeddings
     )
 
-    # It should create documents smaller than the parent
-    # child_splitter = RecursiveCharacterTextSplitter(chunk_size=75, chunk_overlap=0)# , add_start_index=True)
-
     # The storage layer for the parent documents
     store = InMemoryStore()
+    
     pd_retriever = ParentDocumentRetriever(
         vectorstore=vectorstore,
         docstore=store,
         child_splitter=child_splitter,
     )
+
+    pd_retriever.add_documents(docs, ids=None)
+    
 
     return pd_retriever
