75a1ee8790b98bf6dce73578394439461d07376d
jackonedev <jackone.action.software@gmail.com>
2024-03-15 16:48:43 -0300

    updated webscrapper main file

--- backend_ui/A_chile_webscrapper.py
+++ backend_ui/A_chile_webscrapper.py
@@ -6,35 +6,38 @@ if __name__ == "__main__":
 
 
     searchs = [
-        ("codigo_de_comercio", [524, 525, 526]),
+        ("codigo_de_comercio", [524, 525, 526]),#, 538]),
         ("companias_de_seguros", [3, 10, 36]),
         ("protocolo_seguridad_sanitaria", [18]),
         ("codigo_sanitario", [112]),
         ("codigo_penal", [470])
     ]
     
-    if False:
-        # Doesn't work
+    if True:
         try:
-            pr_retriever = parent_document_retriever(searchs)
+            # Doesn't work on Windows
+            pd_retriever = parent_document_retriever(searchs)
         except Exception as e:
             print(e)
             
     
-    search_type = "similarity_score_threshold"
-    kwargs = dict(k=2, score_threshold=0.8)
+    # params 1
+    # search_type = "similarity_score_threshold"
+    # kwargs = dict(k=2, score_threshold=0.8)
+    # retriever = qdrant_retriever(searchs, search_type, kwargs=kwargs)
     
-    retriever = qdrant_retriever(searchs, search_type, kwargs=kwargs)
-    
+    search_type="mmr"
+    search_kwargs={'k': 3, 'lambda_mult': 0.25}
+    retriever = qdrant_retriever(searchs, search_type, kwargs=search_kwargs)
     
     with open("retriever.txt", "a") as f:
         
         msg = input("query for retriever: ")
         response = retriever.invoke(msg)
-        # response = vectorstore.similarity_search_with_score(msg)
-        
         f.write(f"query: {msg}\n")
-        f.write(f"response: {response}\n")
+        f.write(f"response qdrant: {response}\n")
+        response = pd_retriever.invoke(msg)        
+        f.write(f"response pr_retriever: {response}\n")
         f.write("\n"+ "="*50+ "\n")
     
     
